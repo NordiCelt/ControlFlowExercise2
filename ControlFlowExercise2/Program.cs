00f7cbfb1383731672c60adfcdb0628211b82c2f
NordiCelt <tribouletr@gmail.com>
2021-09-07 06:16:04 -0500

    Add project files.

---
+++ ControlFlowExercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ControlFlowExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number, any number: ");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Give me another number: ");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            if (numberOne > numberTwo)
+            {
+                Console.WriteLine($"{numberOne} is larger.");
+            }
+            else
+            {
+                Console.WriteLine($"{numberTwo} is larger.");
+            }
+        }
+    }
+}
